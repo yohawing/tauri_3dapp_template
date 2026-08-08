db1a06157a73e5624167fefa70c632d58f9c36f8
yohawing <yohawing@gmail.com>
2026-08-09 08:02:28 +0900

    Add shell visual comparison harness

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ShellComparisonPage } from "./design/ShellComparisonPage";
+import { ShellReference } from "./design/ShellReference";
 import "./index.css";
+
+const shellMode = new URLSearchParams(window.location.search).get("shell");
+const content = shellMode === "reference"
+  ? <ShellReference />
+  : shellMode === "compare"
+    ? <ShellComparisonPage />
+    : <App />;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {content}
   </React.StrictMode>,
 );
